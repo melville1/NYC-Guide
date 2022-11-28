a0a25da52ae734c18a48b6f906729ec3cb15590f
Melvin <morel.melvin@gmail.com>
2022-11-28 17:52:14 -0500

    final

--- NYCGuide/nycAPP/boroughs.py
+++ NYCGuide/nycAPP/boroughs.py
@@ -1,118 +1,143 @@
 boroughs = {
     'brooklyn': {
-        'beaches': {
-            'brighton beach': { 
-                'img_link': 'https://assets3.thrillist.com/v1/image/2829151/1584x1066/scale;webp=auto;jpeg_quality=60;progressive.jpg',
-                'description': 'Also known as "Little Odessa" due its tight-knit Russian and Eastern European communities, Brooklyn\'s Brighton Beach is a lively neighborhood with many high-rise residential buildings. \ Traditional ethnic restaurants and food markets line Brighton Beach Avenue. The beach and boardwalk here are more laid-back than nearby Coney Island, catering largely to locals. Splashy nightclubs attract partiers in the evenings.'
+        'Places to Eat': {
+            'Birria Landia': { 
+                'img_link': 'https://thebirrialandia.com/wp-content/uploads/2021/02/26REST-TACO-slide-XJ5E-superJumbo-1.jpg',
+                'description': 'Birrieria estilo Tijuana'
             },
-            'coney island': {
-                'img_link': '',
-                'description': 'Coney Island is a residential Brooklyn neighborhood that morphs into a relaxation and entertainment destination each summer. Locals and tourists crowd its beach, the Wonder Wheel and Luna Park, an amusement park featuring the famed Cyclone roller coaster. Street performers, the Circus Sideshow and the Mermaid Parade in June lend an eccentric vibe. Nathan\'s Famous is known for its July 4th hot-dog eating contest.'
+            'Time Out Market': {
+                'img_link': 'https://lh3.googleusercontent.com/p/AF1QipPELzq4x2I3hFl7ocSLCTkXtE1CH0HgMcQ1vWFK=s1360-w1360-h1020',
+                'description': 'New York\’s destination for eating and drinking, featuring the city’s top chefs, most-talked-about restaurants and the buzziest bars—plus the latest in arts and culture all huddled together under one roof.'
             },
-            'manhattan beach': {
-                'img_link': '',
-                'description': 'Manhattan Beach is a laid-back South Bay community popular with families and outdoor enthusiasts. The Strand bike path winds along the oceanfront between modern mansions and wide stretches of sand lined with volleyball courts. Low-key Manhattan Beach Pier features a quaint aquarium and views of surfers. There are several parks, and a small downtown area with boutiques, brunch cafes, pubs & creative, upscale eateries.'
+            'Maison Premiere': {
+                'img_link': 'https://www.liquor.com/thmb/oMFhiJ6INpQtKWePhlGAbknOvnw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/maison-premiere_main_720x7201-8e4c744cff6648f4b79cb45552a87390.jpg',
+                'description': 'Inspired by the elegance and allure of New Orleans, this gorgeous salon—its green walls fogged with a faux patina that suggests decades of Gauloises smoke—is devoted to the twin pleasures of oysters and absinthe: two French Quarter staples with plenty of appeal in Brooklyn'
             }
+    
+        },
+        'Things to do': {
+            'Westlight Rooftop':{
+                'img_link':'https://res.cloudinary.com/traveltripperweb/image/upload/c_fit,f_auto,h_1200,q_auto,w_1200/v1584352112/yrlcvunl1n7vtcwwbuyw.jpg',
+                'description': 'Westlight is a 22nd floor rooftop bar featuring sweeping, 360-degree views of New York City atop The William Vale.' 
+                },
+            'Botanical Garden':{
+                'img_link':'https://www.istoregreen.com/wp-content/uploads/2022/03/Brooklyn-Botanic-Gardens-1024x683.jpg',
+                'description': 'The botanic garden is a must see for every New Yorker and a staple of Brooklyn culture'
+                },
+            'The Watertower Bar':{
+                'img_link':'https://www.behindthescenesnyc.com/wp-content/uploads/2019/09/Nightlife-Bars-The-Water-Tower-Williamsburg-Hotel-NYC-Cocktail-Bar.jpg',
+                'description': 'The Water Tower Bar: Hidden NYC Cocktail Bar High Up In The Sky'
+                },
+            
+
+            
+
+
         }
     },
     'bronx': {
-        'beaches': {
-            'orchard beach': {
-                'img_link': '',
-                'description': 'Orchard Beach, Bronx\'s sole public beach, was proclaimed "The Riviera of New York" when it was created in the 1930s. The 115-acre, 1.1-mile-long beach contains a hexagonal-block promenade, a central pavilion, snack bars, food and souvenir carts, two playgrounds, two picnic areas, a large parking lot, and 26 courts for basketball, volleyball, and handball. Changing areas and showers are available.'
+        'Places to eat': {
+            'Ajo y oregano': {
+                'img_link': 'https://ajoyoregano.com/wp-content/uploads/2018/06/Ajo-y-Oregano_6334_preview.jpeg',
+                'description': 'Ajo y Oregano located at 1556 A White Plains Road, Bronx, NY 10462 serves up traditional style Dominican Food. For many of our patrons, we offer an escape from the busy New York lifestyle back to the home roots of the Dominican Republic. The campo style theme decor and the traditional D.R. dishes bring individuals from all over the country continuing to come back for more. If you\'re looking to satisfy your cravings for authentic, delicious Dominican food - stop in or order online. Either way, we look forward to serving you!'
+            },
+            'Cocina Chente': {
+                'img_link': ' https://lh5.googleusercontent.com/p/AF1QipPiKLStw_I2XQ6srhwEufm0-OxBYkF90Sig7yFJ=w408-h306-k-no',
+                'description': 'Artisanal Mexican cuisine is so much more than authentic dishes and spices; it\’s a vibrant, lovingly preserved food made by our food artisans. Here at Cocina Chente Mezcaleria y Botanas we use fresh and healthy ingredients that will help you to distinguish the artisanal Mexican dishes '
+
+
             }
         },
-        'zoos': {
-            'bronx zoo': {
-                'img_link': '',
-                'description': 'The Bronx Zoo is a zoo located within Bronx Park in the Bronx, New York. It is one of the largest zoos in the United States by area, and is the largest metropolitan zoo in the United States by area, comprising 265 acres of park lands and naturalistic habitats separated by the Bronx River.'
+        'Things to do': {
+            'Bronx botanical garden': {
+                'img_link': 'https://lh3.googleusercontent.com/p/AF1QipPQEs2bukP2rGBmPb_7sKKCoTyYK0eCei71XUuf=s680-w680-h510',
+                'description': 'The New York Botanical Garden is an advocate for the plant world. The Garden pursues its mission through its role as a museum of living plant collections arranged in gardens and landscapes across its National Historic Landmark site; through its comprehensive education programs in horticulture and plant science; and through the wide-ranging research programs of the International Plant Science Center'
+            },
+            'The Bronx Museum of the Arts':{
+                'img_link':'https://arquitectonica.com/architecture/wp-content/uploads/sites/2/2018/07/Bronx_Museum_4-1920x1080.jpg',
+                'description':'The Bronx Museum of the Arts (BxMA), also called the Bronx Museum of Art[2] or simply the Bronx Museum,[3] is an American cultural institution located in Concourse, Bronx, New York. The museum focuses on contemporary and 20th-century works created by American artists, but it has hosted exhibitions of art and design from Latin America, Africa, and Asia. Its permanent collection consists of more than 800 paintings, sculptures, photographs, and works on paper.[4] The museum is part of the Grand Concourse Historic District', 
+
+
             }
         }
     },
     'manhattan': {
-        'food': {
-            'cheeky sandwiches': {
-                'img_link': '',
-                'description': 'A hip sandwich shop offering a select menu of po’ boys & other Big Easy bites in a tiny storefront.'
+        'Places to eat': {
+            'Spicy Moon': {
+                'img_link': 'https://images.squarespace-cdn.com/content/v1/6179a921e2f0103b2a8c61d9/f162b50b-3b64-4161-b25a-18ac191ad608/spicy2.jpeg?format=2500w',
+                'description': 'Spicy Moon is a relatively low-key whole-in-the-wall joint in the East Village, and actually happens to be right in the heart of the Ukrainian village. The place is rather small and casual, but nonetheless has a very comfortable and home-like vibe. The service and owners seem very caring, pleasant, and overall the vibe left a positive impression on me upon entry.'
             },
             'katz\'s delicatessen': {
-                'img_link': '',
+                'img_link': 'https://www.fromthesquare.org/wp-content/uploads/2014/11/Katzs_Delicatessen-960x754.jpg',
                 'description': 'No-frills deli with theatrically cranky service serving mile-high sandwiches since 1888.'
             },
-            'veselka': {
-                'img_link': '',
-                'description': 'Borscht & pierogi are menu highlights at this cheap, no-frills Ukrainian eatery open 24 hours a day.'
-            }
+            
         },
-        'parks': {
+        'Things to do': {
             'central park': {
-                'img_link': '',
+                'img_link': 'https://lp-cms-production.imgix.net/2020-09/shutterstockRF_519864937.jpg?auto=format&q=40&ar=16%3A9&fit=crop&crop=center&fm=auto&w=799',
                 'description': 'Central Park is an urban park in New York City located between the Upper West and Upper East Sides of Manhattan. It is the fifth-largest park in the city by area, covering 843 acres.'
             },
-            'riverside park': {
-                'img_link': '',
-                'description': 'Riverside Park is a scenic waterfront public park in the Upper West Side, Morningside Heights, and Hamilton Heights neighborhoods of the borough of Manhattan in New York City.'
+            'Whitney Museum': {
+                'img_link': 'https://www.silman.com/wp-content/uploads/2020/04/Whitney-Museum-03-Cantilever.jpg',
+                'description': 'The Whitney focuses on 20th- and 21st-century American art. Its permanent collection, spanning the late-19th century to the present, comprises more than 25,000 paintings, sculptures, drawings, prints, photographs, films, videos, and artifacts of new media by more than 3,500 artists. It places particular emphasis on exhibiting the work of living artists as well as maintaining an extensive permanent collection of important pieces from the first half of the last century. '
             },
-            'the highline':  {
-                'img_link': '',
-                'description': 'The High Line is a 1.45-mile-long elevated linear park, greenway and rail trail created on a former New York Central Railroad spur on the west side of Manhattan in New York City. The High Line\'s design is a collaboration between James Corner Field Operations, Diller Scofidio + Renfro, and Piet Oudolf.'
+            'The Edge':  {
+                'img_link': 'https://s3.amazonaws.com/images.certifikid.com/content_images/6/1220a_The-Edge-2.jpg',
+                'description': 'The Edge at Hudson Yards is the highest outdoor sky deck in the Western Hemisphere, with a one-of-a-kind design. It’s suspended in mid-air, giving you the feeling of floating in the sky with 360-degree views you can’t get anywhere else.\n Look 100 stories down from the thrilling glass floor, lean out over the city on angled glass walls and sip champagne in the sky. You’ve never experienced New York like this before'
             },
-            'washington square park':  {
-                'img_link': '',
-                'description': 'Washington Square Park is a 9.75-acre public park in the Greenwich Village neighborhood of Lower Manhattan, New York City. One of the best known of New York City\'s public parks, it is an icon as well as a meeting place and center for cultural activity.'
-            }
-        }
+            
+             }
     },
     'queens': {
-        'airports': {
-            'john f kennedy international airport': {
-                'img_link': '',
-                'description': 'John F. Kennedy International Airport is an international airport in Queens, New York, USA. It is the primary international airport serving New York City.'
+        'Places to eat': {
+            'The Freakin Rican': {
+                'img_link': 'https://thefreakinricanrestaurant.com/wp-content/uploads/sb-instagram-feed-images/274787770_1686046308415348_704131015170022261_n.webpfull.jpg',
+                'description': 'The Freakin Rican brand is dedicated to serving the most delicious freshest food, second to none. We serve our community with pride and work continuously creating food products that are healthier and showcase our heritage.'
             },
-            'laguardia airport': {
-                'img_link': '',
-                'description': 'LaGuardia Airport is an airport in Queens, New York. The airport is the third busiest airport serving New York City, and the twentieth busiest in the United States. LaGuardia Airport covers 680 acres.'
+            'Blend on the water': {
+                'img_link': 'https://resizer.otstatic.com/v2/photos/wide-xlarge/1/47043913.jpg',
+                'description': 'Latin fusion restaurant, presenting authentic cuisine inspired by all Latin countries.The Music, The Art, The Culture, The People, The Food. Blend is the ultimate dining experience.'
             }
         },
-        'beaches': {
-            'rockaway beaches': {
-                'img_link': '',
-                'description': 'From surfers to swimmers to the Ramones, everyone wants to "hitch a ride to Rockaway Beach." Rockaway Beach is a year-round resource for residents all along the Rockaway peninsula, and it comes alive each summer with millions of visitors. Beach goers can enjoy the sand and water, the variety of concessions, and the city\'s only legal surfing beaches. Rockaway Beach is also home to a variety of playgrounds and other outdoor activities.'
-            }
-        }
+        'Things to do': {
+            'Museum of the Moving Image': {
+                'img_link': 'https://media.timeout.com/images/101748989/image.jpg',
+                'description': 'The Museum of the Moving Image is one of the city’s most dynamic institutions. Rubbing elbows with Kaufman Astoria Studios, it includes a three-story extension that features a state-of-the-art 267-seat cinema and expanded gallery spaces.'
+            },
+            'Gantry Plaza State Park': {
+                'img_link': 'https://i.pinimg.com/originals/b4/11/a5/b411a5320105ec4b6a3adc46ec254a6a.jpg',
+                'description': 'Located on 47th Road, Gantry Plaza State Park, also known simply as Gantry Park, sits along the East River in the Hunters Point area of Long Island City. Opened in 1998 and expanded in 2009, it is a 10-acre park with a playground, playing fields, picnic tables, and a riverside walk that offers great, unobstructed views of the midtown Manhattan skyline as well as the United Nations Building',
+
+
+            },
+        },
 
     },
     'staten island': {
-        'beaches': {
-            'cedar grove beach': {
-                'img_link': '',
-                'description': 'The city\'s newest beach is a converted oceanfront bungalow colony. This charming beach is smaller and somewhat more tranquil than its neighbors, Midland Beach and South Beach, and is popular with the area\'s families.'
+        'Places to eat': {
+            'Luce NY': {
+                'img_link': 'https://static01.nyt.com/images/2014/08/31/nyregion/31DINECT1/31DINECT1-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
+                'description': 'A modern mix of fine Italian cuisine, atmosphere and culture. Let us share our passion with you while we light the way to an exquisite dining experience.'
             },
-            'midland beach': {
-                'img_link': '',
-                'description': 'Midland Beach shares the Franklin D. Roosevelt Boardwalk with neighboring South Beach, and tends to be the somewhat less crowded of the two beaches. The beach\'s attractions include a playground and courts for tennis and shuffleball, as well as a sea turtle fountain for children.'
+            'Fiore di Maresi': {
+                'img_link': 'https://images.squarespace-cdn.com/content/v1/5bd486afaadd346a0cfbd0cf/1542651714550-WE8KDRGQ0F6LG67PCZUB/Fiore+di+Mare+-+NYC+Italian+Restaurant+-+Lobster.jpg?format=1500w',
+                'description': 'a hidden gem on the edge of Staten Island offering a tasteful blend of classic and contemporary Italian dishes in a familiar cozy atmosphere.'
             },
-            'south beach': {
-                'img_link': '',
-                'description': 'Beachgoers can sunbathe while taking in a lovely view of the Verazzano Bridge, bike through a scenic trail, jog along the boardwalk, kayak, play tennis, or fish off of the Ocean Breeze Fishing Pier, one of the city\'s most popular. A park on the beach designed especially for seniors contains chess tables, benches, and bocce courts. The beach\'s Fountain of Dolphins is a popular stop on any tour of the boardwalk.'
-            },
-            'wolfe\'s pond beach': {
-                'img_link': '',
-                'description': 'Love the ocean but think NYC’s beaches are just too chaotic? Wolfe’s Pond Beach is the city’s best-kept secret, a small, calm, and secluded beach that’s a perfect retreat for those looking to get away from summer’s crowds.'
-            }
+            
         },
-        'food': {
-            'lee\'s tavern': {
-                'img_link': '',
-                'description': 'Family-run neighborhood pub with photos on the wall serving a range of pizza & brews.'
+        'Things to do': {
+            'Roller Jam USA': {
+                'img_link': 'https://lh3.googleusercontent.com/p/AF1QipMJbw66KJDV5-2Lvyy7OgYg1DVmc8b-vBQ0doCj=s680-w680-h510',
+                'description': 'A new twist on an old idea is what made RollerJam an overnight success. A new skate floor design, with a night club concept in mind, while presenting its perfectly aligned sound system and new technology disco light display “WoW’d!” the new roller skating patrons on the floor'
             },
             'ralph\'s famous italian ices': {
-                'img_link': '',
+                'img_link': 'https://media-cdn.tripadvisor.com/media/photo-s/0e/a8/8e/ce/photo0jpg.jpg',
                 'description': 'Staten Island-born chain dispensing Italian ices & ice cream in 100-plus flavors since 1928.'
             },
-            'royal crown bakery': {
-                'img_link': '',
-                'description': 'A local Italian bakery serves bread, pastries & coffees, with an adjacent cucina for hot meals.'
+            'Snug Harbor Cultural Center': {
+                'img_link': 'https://assets.simpleviewinc.com/simpleview/image/upload/crm/newyorkstate/snugharbor_taggeryanceyiv_4105snugharbor_taggeryanceyiv_41051_af256d35-9fbe-fe27-1abb2609ccf31ce6.jpg',
+                'description': 'Snug Harbor Cultural Center & Botanical Garden is the result of more than four decades of restoration and development to convert a 19th century charitable rest home for sailors to a regional arts center, botanical gardens, and public park. It is a place where history, architecture, gardens, agriculture, visual and performing arts, and education come together to provide a dynamic experience for all ages.'
             }
         }
     },
